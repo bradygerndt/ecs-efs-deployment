00743764fa2e1baa84fe1b375ed6acf6988c858d
Brady <brady.gerndt@gmail.com>
2020-05-02 11:48:05 -0500

    Set up provider and variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_regions" {
+  type        = list(string)
+  default     = ["us-east-1", "us-east-2"]
+  description = "AWS regions"
+}
+
+variable "default_tags" {
+  type = map(string)
+  default = {
+      Terraform = true
+      Project = "factorio-tf"
+  }
+}
+
