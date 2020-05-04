88971f8f828498883c97c6da008e8802f2b8239a
unknown <brady.gerndt@gmail.com>
2020-05-03 23:41:03 -0500

    Fixed variables in ecs_service definition due to string interpolation deprecation

--- ecs-service.tf
+++ ecs-service.tf
@@ -1,13 +1,12 @@
 resource "aws_ecs_service" "factorio" {
   name            = "factorio"
-  cluster         = "aws_ecs_cluster.factorio-cluster.id"
-  task_definition = "aws_ecs_task_definition.factorio.arn"
+  cluster         = aws_ecs_cluster.factorio-cluster.id
+  task_definition = aws_ecs_task_definition.factorio.arn
   desired_count   = 1
   platform_version = "1.4.0"
   launch_type = "FARGATE"
 
   network_configuration {
-
     subnets          = [aws_subnet.main.id]
     assign_public_ip = true
     security_groups  = [aws_security_group.allow_factorio.id]
