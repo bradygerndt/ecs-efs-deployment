2561824b67472c4adf20d8bc7a0ee01b9e515956
unknown <brady.gerndt@gmail.com>
2020-05-10 18:57:06 -0500

    Bounds are annoying, trying again

--- autoscaling.tf
+++ autoscaling.tf
@@ -18,7 +18,7 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
     cooldown                = 60
     metric_aggregation_type = "Average"
     step_adjustment {
-      metric_interval_lower_bound = 0
+      metric_interval_lower_bound = 10
       scaling_adjustment          = 0
     }
   }
