99335aaa9bf043a339e1572eb9af9e0e3ae8bcac
unknown <brady.gerndt@gmail.com>
2020-05-10 19:07:45 -0500

    Trying again

--- autoscaling.tf
+++ autoscaling.tf
@@ -18,11 +18,12 @@ resource "aws_appautoscaling_policy" "ecs_policy" {
     cooldown                = 60
     metric_aggregation_type = "Average"
     step_adjustment {
-      metric_interval_lower_bound = 0
+      metric_interval_upper_bound = 0
       scaling_adjustment          = 0
     }
   }
 }
+
 
 resource "aws_cloudwatch_metric_alarm" "ECS-LOW-CPU" {
   alarm_name          = "ECS-LOW-CPU-UTILIZATION"
